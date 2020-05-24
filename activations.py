723bc2e743c2bd549c51e412029412ea818dc6d2
Seongmin Park <llov0708@gmail.com>
2020-05-24 16:21:56 +0900

    Add initial activations

--- activations.py
+++ activations.py
@@ -1,7 +1,17 @@
 import numpy as np
 
+def str2act(name):
+    if name == 'sigmoid':
+        return sigmoid
+    elif name == 'relu':
+        return relu
+    else:
+        return None
+
 def sigmoid():
     return 1
 
 def relu():
     return 1
+
+
